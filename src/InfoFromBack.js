ddc53240de6072766ac00814d6c9182bfffc8cbc
ALEKSANDR399 <88842048+ALEKSANDR399@users.noreply.github.com>
2022-02-19 18:51:39 +0200

    ДЗ№11

---
+++ src/InfoFromBack.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+export default class NewsApiService {
+  constructor() {
+    this.info = '';
+    this.page = 1;
+  }
+
+  async getUser() {
+    console.log(this);
+    const BASE_URL = 'https://pixabay.com/api';
+    const MY_KEY = 'key=25767456-f92e672e1b91dd336dc5b7edd';
+    const CONST_PARAM = `image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
+
+    const response = await axios.get(`${BASE_URL}?${MY_KEY}&q=${this.info}&${CONST_PARAM}`);
+    this.incrementPage();
+    return response;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+  get informationSearch() {
+    return this.info;
+  }
+  set informationSearch(newInfo) {
+    this.info = newInfo;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
 import './css/styles.css';
+import imgCards from './templates/cards.hbs';
+import Notiflix from 'notiflix';
+import NewsApiService from './InfoFromBack';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const formSearchRef = document.querySelector('.search-form');
+const galleryBox = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+const newsApiService = new NewsApiService();
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+const allCardsEl = galleryBox.getElementsByTagName('a');
+
+formSearchRef.addEventListener('submit', addSearchInfo);
+loadMoreBtn.addEventListener('click', addMoreImg);
+
+async function addSearchInfo(event) {
+  event.preventDefault();
+  galleryBox.innerHTML = '';
+
+  newsApiService.informationSearch = event.currentTarget.elements.searchQuery.value;
+  newsApiService.resetPage();
+
+  const infoSearch = await newsApiService.getUser();
+  const { data } = infoSearch;
+  const { hits, totalHits } = data;
+
+  if (newsApiService.info === '') {
+    loadMoreBtn.classList.add('is-hidden');
+    return;
+  }
+
+  if (hits.length === 0) {
+    loadMoreBtn.classList.add('is-hidden');
+    return Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.',
+    );
+  }
+  loadMoreBtn.classList.remove('is-hidden');
+  Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+  galleryBox.insertAdjacentHTML('beforeend', imgCards(hits));
+  lightbox.refresh();
+}
+
+async function addMoreImg() {
+  const infoSearch = await newsApiService.getUser();
+  const { data } = infoSearch;
+  const { hits } = data;
+
+  galleryBox.insertAdjacentHTML('beforeend', imgCards(hits));
+  lightbox.refresh();
+  if (data.totalHits === allCardsEl.length) {
+    loadMoreBtn.classList.add('is-hidden');
+    Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+  }
+}
